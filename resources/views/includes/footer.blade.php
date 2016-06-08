98869a8df58520901daca01d62e893180bceeca7
picassofb <picassofb@hotmail.com>
2016-06-08 21:36:44 +0000

    Layout inical

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,64 @@
+
+<!-- BEGIN FOOTER -->
+<div class="page-footer">
+    <div class="page-footer-inner"> 2015 &copy; Metronic by keenthemes.
+        <a href="http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" title="Purchase Metronic just for 27$ and get lifetime updates for free" target="_blank">Purchase Metronic!</a>
+    </div>
+    <div class="scroll-to-top">
+        <i class="icon-arrow-up"></i>
+    </div>
+</div>
+<!-- END FOOTER -->
+
+        <!-- BEGIN CORE PLUGINS -->
+<script src="{{ URL::to('plugins/jquery.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/js.cookie.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
+<!-- BEGIN PAGE LEVEL PLUGINS -->
+<script src="{{ URL::to('plugins/moment.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/bootstrap-daterangepicker/daterangepicker.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/morris/morris.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/morris/raphael-min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/counterup/jquery.waypoints.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/counterup/jquery.counterup.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/amcharts/amcharts.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/amcharts/serial.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/amcharts/pie.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/amcharts/radar.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/amcharts/themes/light.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/amcharts/themes/patterns.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/amcharts/themes/chalk.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/ammap/ammap.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/ammap/maps/js/worldLow.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/amcharts/amstockcharts/amstock.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/fullcalendar/fullcalendar.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/horizontal-timeline/horozontal-timeline.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/flot/jquery.flot.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/flot/jquery.flot.resize.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/flot/jquery.flot.categories.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jquery-easypiechart/jquery.easypiechart.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jquery.sparkline.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jqvmap/jqvmap/jquery.vmap.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js') }}" type="text/javascript"></script>
+<!-- END PAGE LEVEL PLUGINS -->
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="{{ URL::to('scripts/app.min.js') }}" type="text/javascript"></script>
+<!-- END THEME GLOBAL SCRIPTS -->
+<!-- BEGIN PAGE LEVEL SCRIPTS -->
+<script src="{{ URL::to('scripts/dashboard.min.js') }}" type="text/javascript"></script>
+<!-- END PAGE LEVEL SCRIPTS -->
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="{{ URL::to('layout/scripts/layout.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('layout/scripts/demo.min.js') }}" type="text/javascript"></script>
+<script src="{{ URL::to('layout/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!-- URL::forceSchema('https') -->
+{{ URL::forceSchema('https') }}
+
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>@yield('titulo')</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="Pesaje CodEd" name="description" />
+        <meta content="Eduardo Salazar" name="author" />
+        <!-- BEGIN GLOBAL MANDATORY STYLES -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::to('plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::to('plugins/simple-line-icons/simple-line-icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::to('plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::to('plugins/bootstrap-switch/css/bootstrap-switch.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- END GLOBAL MANDATORY STYLES -->
+        <!-- BEGIN PAGE LEVEL PLUGINS -->
+        <link href="{{ URL::to('plugins/bootstrap-daterangepicker/daterangepicker.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::to('plugins/morris/morris.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::to('plugins/fullcalendar/fullcalendar.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::to('plugins/jqvmap/jqvmap/jqvmap.css') }}" rel="stylesheet" type="text/css" />
+        <!-- END PAGE LEVEL PLUGINS -->
+        <!-- BEGIN THEME GLOBAL STYLES -->
+        <link href="{{ URL::to('css/components.min.css') }}" rel="stylesheet" id="style_components" type="text/css" />
+        <link href="{{ URL::to('css/plugins.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- END THEME GLOBAL STYLES -->
+        <!-- BEGIN THEME LAYOUT STYLES -->
+        <link href="{{ URL::to('layout/css/layout.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ URL::to('layout/css/themes/blue.min.css') }}" rel="stylesheet" type="text/css" id="style_color" />
+        <link href="{{ URL::to('layout/css/custom.min.css') }} " rel="stylesheet" type="text/css" />
+        <!-- END THEME LAYOUT STYLES -->
+        <link rel="shortcut icon" href="{{ URL::to('favicon.ico') }}" /> 
+        
+        @yield('styles')
+    </head>
+    
+    <body class="page-header-fixed page-sidebar-closed-hide-logo page-container-bg-solid">
+        
+        
+        @include('includes.header')
+        
+         
+        <!-- BEGIN HEADER & CONTENT DIVIDER -->
+        <div class="clearfix"> </div>
+        <!-- END HEADER & CONTENT DIVIDER -->
+        
+        <!-- BEGIN CONTAINER -->
+        <div class="page-container">
+            @include('includes.sideMenu')
+            
+            <div class="page-content-wrapper">
+                <!-- BEGIN CONTENT BODY -->
+                <div class="page-content">
+                    
+                    
+                        <h3 class="page-title"> Dashboard
+                            <small>dashboard & statistics</small>
+                        </h3>
+                        <div class="page-bar">
+                            <ul class="page-breadcrumb">
+                                <li>
+                                    <i class="icon-home"></i>
+                                    <a href="index.html">Home</a>
+                                    <i class="fa fa-angle-right"></i>
+                                </li>
+                                <li>
+                                    <span>Dashboard</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- END PAGE HEADER-->
+                    
+                    
+                    <div class="row">
+                        <div class="col-lg-12 col-md-3 col-sm-6 col-xs-12">
+                            @yield('contenido')
+                        </div>
+                    </div>    
+                    
+                </div>
+            </div>
+            
+    
+        </div>
+        <!-- END CONTAINER -->
+
+        
+         @include('includes.footer')
+    </body>
+    
+</html>
